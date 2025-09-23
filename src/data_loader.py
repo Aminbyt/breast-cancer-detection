4976c9853427f63abc5c4b8c15f1ab98b95b300c
aminbyt <m.aminbayat80@gmail.com>
2025-09-24 00:48:43 +0330

    Update data_loader to automatically locate dataset in project folder

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,18 @@
+from tkinter import N
 import pandas as pd 
+import os
 
-def load_data(path=r"data/Breast_cancer_dataset.csv"):
-    return pd.read_csv(path)
+def load_data(path=None):
+    if path is None:
+        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        csv_path = os.path.join(project_root, "data", "Breast_cancer_dataset.csv")
+    else:
+        csv_path = path
+
+    csv_path = os.path.abspath(csv_path)
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(
+            f"CSV not found at: {csv_path}\nCurrent working dir: {os.getcwd()}"
+        )
+
+    return pd.read_csv(csv_path)
